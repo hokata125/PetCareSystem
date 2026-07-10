f37cd5e27aa24b8b1e9b69816d3a7fe75d472de2
hokata125 <khangth125@gmail.com>
2026-07-11 01:55:25 +0700

    update models.py

--- app/core/configs.py
+++ app/core/configs.py
@@ -4,7 +4,6 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     
-    # Cấu hình Cloudinary
     CLOUDINARY_CLOUD_NAME: str
     CLOUDINARY_API_KEY: str
     CLOUDINARY_API_SECRET: str
--- app/models/models.py
+++ app/models/models.py
@@ -129,6 +129,9 @@ class Booking(Base):
     service_id = Column(Integer, ForeignKey("services.id"), nullable=False)
     booking_date = Column(DateTime, nullable=False)
 
+    start_at = Column(DateTime, nullable=False)
+    duration_minutes = Column(Integer, nullable=True)
+    end_at = Column(DateTime, nullable=True)
 
     pet_name = Column(String(100), nullable=False)
     pet_type = Column(String(50), nullable=False)

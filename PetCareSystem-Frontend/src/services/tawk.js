6470d0809d3d13e96646ffc7c42188f523c82f6e
hokata125 <khangth125@gmail.com>
2026-08-20 17:26:53 +0700

    update tawk

--- PetCareSystem-Frontend/src/services/tawk.js
+++ PetCareSystem-Frontend/src/services/tawk.js
@@ -6,9 +6,7 @@ export const getTawkIdentity = async () => {
 };
 
 const runWhenTawkIsReady = (action) => {
-  if (!window.Tawk_API) {
-    window.Tawk_API = {};
-  }
+  window.Tawk_API = window.Tawk_API || {};
 
   if (typeof window.Tawk_API.login === "function") {
     action();
@@ -20,16 +18,24 @@ const runWhenTawkIsReady = (action) => {
 
 export const loginTawkUser = (tawkIdentity) => {
   runWhenTawkIsReady(() => {
-    window.Tawk_API.login({
-      userId: tawkIdentity.user_id,
-      hash: tawkIdentity.hash,
-      name: tawkIdentity.name,
-    });
+    window.Tawk_API.login(
+      {
+        userId: tawkIdentity.user_id,
+        hash: tawkIdentity.hash,
+        name: tawkIdentity.name,
+      },
+      (error) => {
+        if (!error) {
+          window.Tawk_API.showWidget();
+        }
+      },
+    );
   });
 };
 
 export const logoutTawkUser = () => {
   runWhenTawkIsReady(() => {
+    window.Tawk_API.hideWidget();
     window.Tawk_API.logout();
   });
 };

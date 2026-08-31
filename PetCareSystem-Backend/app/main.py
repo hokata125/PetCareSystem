b9fc5a7e45dac6e87eae63f3e8f1be018111ae07
hokata125 <khangth125@gmail.com>
2026-09-01 02:50:55 +0700

    add CORS ou pet center vercel

--- PetCareSystem-Backend/app/main.py
+++ PetCareSystem-Backend/app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://ou-pet-center.vercel.app",
     ],
     allow_credentials=False,
     allow_methods=["*"],
